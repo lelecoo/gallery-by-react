4b710ffc07e75fc65fce8af3bc975db060038ea0
lelecoo <601877602@qq.com>
2018-02-24 09:52:39 +0800

    Git初试 创建仓库

---
+++ src/main/java/Controller.java
@@ -0,0 +1,14 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by lele on 2018/2/11 0011.
+ */
+public class Controller extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException{
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World ,this is Maven project!</h2>
+</body>
+</html>
